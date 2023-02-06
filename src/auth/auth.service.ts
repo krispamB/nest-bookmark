05a0051718397b7edfc994366fb1777f1fe80e58
Christopher Pam <chrystopam@gmail.com>
2023-02-06 14:00:16 +0100

    created signup function

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { errorMonitor } from 'events';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  signup() {
-    return { msg: 'Hello, I am signed up' };
+  async signup(dto: AuthDto) {
+    //Generate Password
+    const hash = await argon.hash(dto.password);
+    // save the user in db
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+      delete user.hash;
+      //return saves user
+      return user;
+    } catch (error) {
+      // Check if error is a prisma error and check if error code is a duplicate key error code(ie 'P2002'). If not throw error
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Cridentials taken');
+        }
+      }
+      throw error;
+    }
   }
 
-  signin() {
+  async signin() {
     return { msg: 'Hello, I am signed in' };
   }
 }
